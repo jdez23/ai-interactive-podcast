13239d7ddc9193531341956d3272ac4d1e9d66c6
Claudia Trejo Hernandez <claudia.s.trejo@gmail.com>
2026-01-03 21:35:22 -0800

    Update Xcode project configuration and DocumentListView

--- frontend/AIPodcast/Views/Documents/DocumentListView.swift
+++ frontend/AIPodcast/Views/Documents/DocumentListView.swift
@@ -9,11 +9,24 @@ struct DocumentListView: View {
     ]
     
     var body: some View {
-        List(placeholderDocuments, id: \.self) { document in
-            HStack {
-                Image(systemName: "doc.text")
-                    .foregroundColor(.blue)
-                Text(document)
+        List {
+            Section {
+                NavigationLink {
+                    GenerateTabView()
+                } label: {
+                    Label("Upload PDFs to Generate", systemImage: "arrow.up.doc")
+                        .foregroundColor(.blue)
+                }
+            }
+            
+            Section("Recent Documents") {
+                ForEach(placeholderDocuments, id: \.self) { document in
+                    HStack {
+                        Image(systemName: "doc.text")
+                            .foregroundColor(.blue)
+                        Text(document)
+                    }
+                }
             }
         }
     }
